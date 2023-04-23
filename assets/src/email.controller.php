95ee209141292669f3c44dd8c96ae322e8557a38
javecilla <jeromesavc@gmail.com>
2023-04-24 00:31:24 +0800

    Update email controller file

--- assets/src/email.controller.php
+++ assets/src/email.controller.php
@@ -11,63 +11,68 @@ if(isset($_POST['name']) && isset($_POST['email'])) {
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
 
-	if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	try {
+    if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 		
-		//create a new object
-		$mail = new PHPMailer();
+			//create a new object
+			$mail = new PHPMailer();
 
-		//enable verbose debug output
-		$mail->SMTPDebug = 2;
-		$mail->Debugoutput = 'html';
+			//enable verbose debug output
+			$mail->SMTPDebug = 2;
+			$mail->Debugoutput = 'html';
 
-		//stmp settings
-		$mail->isSMTP();
+			//stmp settings
+			$mail->isSMTP();
 
-		//for local
-		// $mail->Host ="ssl://smtp.gmail.com"; 
-		// $mail->Port = 465; 
-		// $mail->SMTPSecure = 'ssl'; 
-		// $mail->SMTPAuth = true;
+			//for local
+			// $mail->Host ="ssl://smtp.gmail.com"; 
+			// $mail->Port = 465; 
+			// $mail->SMTPSecure = 'ssl'; 
+			// $mail->SMTPAuth = true;
+
+			//for live
+			$mail->Host ="tls://smtp.gmail.com"; 
+			$mail->Port = 587; 
+			$mail->SMTPSecure = 'tls'; 
+			$mail->SMTPAuth = true;
+
+			//retrieve credentials
+			$mail->Username = EMAIL;
+			$mail->Password = PASS;
+
+			//email settings
+			$mail->isHTML(true);
+			$mail->ContentType = 'text/plain';
+			$mail->CharSet ="utf-8";
+
+			$mail->setFrom($email, $name);
+			$mail->addAddress(EMAIL);
+			$mail->Subject = ("$email ($subject)");
+			$mail->Body = $message;
+
+			//for debugging for purpose
+	    $mail->SMTPOptions = array(
+	      'ssl' => array(
+	        'verify_peer' => false,
+	        'verify_peer_name' => false,
+	        'allow_self_signed' => true
+	      )
+	    );
+
+	    //check mailing
+		  if($mail->send()){
+		   	//echo "Email sent successfully!";
+		  } else {
+		  	throw new Exeption("Failed to send email: " . $mail->ErrorInfo);
+		  }
+
+		} else {
+			throw new Exeption("Invalid email address");
+		}
 		
-		//for live
-		$mail->Host ="tls://smtp.gmail.com"; 
-		$mail->Port = 587; 
-		$mail->SMTPSecure = 'tls'; 
-		$mail->SMTPAuth = true;
-
-		//retrieve credentials
-		$mail->Username = EMAIL;
-		$mail->Password = PASS;
-
-		//email settings
-		$mail->isHTML(true);
-		$mail->ContentType = 'text/plain';
-		$mail->CharSet ="utf-8";
-
-		$mail->setFrom($email, $name);
-		$mail->addAddress(EMAIL);
-		$mail->Subject = ("$email ($subject)");
-		$mail->Body = $message;
-
-		//for debugging for purpose
-    $mail->SMTPOptions = array(
-      'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
-      )
-    );
-
-    //check mailing
-    if($mail->send()){
-    	//echo "Email sent successfully!";
-    } else {
-    	echo "Failed to send email: " . $mail->ErrorInfo;
-    }
-
-	} else {
-		echo "Invalid Email address";
-	}
+	} catch(Exeption $e) {
+		echo "An error occured: " . $e->getMessage();
+	}	
 }
 
 ?>
